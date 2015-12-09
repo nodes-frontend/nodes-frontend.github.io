e1c55758161980becb1ac1ce9ec10a1818acd1c1
Dennis Haulund Nielsen <dhni@nodes.dk>
2015-12-09 09:57:56 +0100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -27,10 +27,12 @@
 
 
 /* eslint-disable quotes, comma-spacing */
-var PrecacheConfig = [["app/common/utils/blog-helpers/BlogHelpers.module.js","76a344bcd481fd02f2b059f41162fa10"],["app/common/utils/blog-helpers/BlogHelpers.service.js","fb138471bcf5c6696ffa7b957ba87117"],["app/common/utils/page-helpers/PageHelpers.module.js","aef59c45707b768fbbab94ff75b7f90e"],["app/common/utils/page-helpers/PageHelpers.service.js","56c2df653d622b95833626d9143d349c"],["app/config/api.constant.js","f9c4d22eaba858b038a84779d91f97b2"],["app/config/api.module.js","a64e6a990b225f308ed8fcf7f10b6720"],["app/config/app.js","10646d66cba646207d98004498bc8af6"],["app/config/config.js","00c4a4ec1525f4fb2205f038a12d8fff"],["app/config/run.js","6bb28aaf8944eb71b04dd65aed9a0f50"],["app/models/page/Page.factory.js","f0db919eac18711e7c7bbda06f3d97eb"],["app/models/page/Pages.module.js","3bc230c469cea219c2c06e53a552c507"],["app/models/page/Pages.service.js","ce9ffdf9b69fcd5c87b211d48bbf3e36"],["app/models/post/Post.factory.js","a3e6e846b55fb018393cbfff179c4c16"],["app/models/post/Posts.module.js","d4916c04bdbafa942946378e3c77cabc"],["app/models/post/Posts.service.js","860e091b3dbcb68369b0ea31de4cc4f1"],["app/models/site/Site.module.js","499acc88cdc4053cdd4e80e9d0196d59"],["app/models/site/Site.service.js","ad63d64ff66adaa35c9dd16830a72b1f"],["app/modules/404/404.controller.js","94c75ed87794e85f37257d88cfaa743a"],["app/modules/404/404.module.js","b7aaba4be35e7fe61f55436120dbea6d"],["app/modules/404/404.route.js","3221ab80e1a3e4935367a16158dc5fef"],["app/modules/404/404.template.html","77ad034aee94eeb925888489526e41cc"],["app/modules/application/application.controller.js","9bef7e577f310809587cf0c59a698cee"],["app/modules/application/application.module.js","b6b91788bd5573bfee35d3b65d337168"],["app/modules/application/application.route.js","b7a082822c872159f328b6dfab9fb00a"],["app/modules/application/application.template.html","38386deea43db8ff3725ce30e16a04af"],["app/modules/blog/blog.controller.js","2d1888be844a4b4c1172a738508684e7"],["app/modules/blog/blog.module.js","fd882fdbbc246c5cff8d677a4d08ac06"],["app/modules/blog/blog.route.js","85f130280326b03c6a7922fa9cb72443"],["app/modules/blog/blog.template.html","7c51911bc61fc794af09fe38553205fa"],["app/modules/blog/single/blog-single.controller.js","b1fc2286df876d9b6c515f3e4bc67458"],["app/modules/blog/single/blog-single.template.html","606ab841f323106aa518749fcc0a2994"],["app/modules/index/index.controller.js","03861f49667b94c268c6a39f6b5d3590"],["app/modules/index/index.module.js","939cecc6fb269349192edb044f2cbf2d"],["app/modules/index/index.route.js","3a58e7bcbb9db92cc53eb30601427976"],["app/modules/index/index.template.html","cbb49c979ff4136c177a0793ed824dc7"],["app/modules/page/page.controller.js","001c008b7717e35eb2fdeb34f9c71e8b"],["app/modules/page/page.module.js","edc66aa3d2be58df76e8a88578e630c4"],["app/modules/page/page.route.js","2c079fd61cba11b5538d523f8a1d9c04"],["app/modules/page/page.template.html","6521fc1ac0a7f34a7d87aa8758b03032"]];
+var PrecacheConfig = [["assets/css/main.min.c4eb67fe.css","c4eb67fe1bd4f4da342162c9499de098"],["assets/fonts/CrimsonText-Bold.ttf","7c5a522c882d6ad8876545b90a4c2b38"],["assets/fonts/CrimsonText-BoldItalic.ttf","3469d9f14554deffd9be95abf588d699"],["assets/fonts/CrimsonText-Italic.ttf","cde5e3a8acb5d68805d01f2702534cf4"],["assets/fonts/CrimsonText-Roman.ttf","ca4e112f807260e7cfba634a51ac6735"],["assets/fonts/CrimsonText-Semibold.ttf","b2aaf0a3a5e884535248d205c5aba309"],["assets/fonts/CrimsonText-SemiboldItalic.ttf","129a5ed794bcb947ec4f7f6aa88c2db9"],["assets/fonts/Montserrat-Bold.ttf","d3085f686df272f9e1a267cc69b2d24f"],["assets/fonts/Montserrat-Regular.ttf","07689d4eaaa3d530d58826b5d7f84735"],["assets/js/site.2331fb43.js","2331fb435c86efa394c31515c7ae418c"],["assets/js/vendor.f1f82df6.js","f1f82df65b3b5fc11cbef2e2908c4fee"]];
 /* eslint-enable quotes, comma-spacing */
 var CacheNamePrefix = 'sw-precache-v1-nodes-frontend-' + (self.registration ? self.registration.scope : '') + '-';
 
+
+var IgnoreUrlParametersMatching = [/^utm_/];
 
 
 
@@ -170,4 +172,49 @@ self.addEventListener('message', function(event) {
   }
 });
 
+
+self.addEventListener('fetch', function(event) {
+  if (event.request.method === 'GET') {
+    var urlWithoutIgnoredParameters = stripIgnoredUrlParameters(event.request.url,
+      IgnoreUrlParametersMatching);
+
+    var cacheName = AbsoluteUrlToCacheName[urlWithoutIgnoredParameters];
+    var directoryIndex = 'index.html';
+    if (!cacheName && directoryIndex) {
+      urlWithoutIgnoredParameters = addDirectoryIndex(urlWithoutIgnoredParameters, directoryIndex);
+      cacheName = AbsoluteUrlToCacheName[urlWithoutIgnoredParameters];
+    }
+
+    var navigateFallback = '';
+    // Ideally, this would check for event.request.mode === 'navigate', but that is not widely
+    // supported yet:
+    // https://code.google.com/p/chromium/issues/detail?id=540967
+    // https://bugzilla.mozilla.org/show_bug.cgi?id=1209081
+    if (!cacheName && navigateFallback && event.request.headers.has('accept') &&
+        event.request.headers.get('accept').includes('text/html')) {
+      var navigateFallbackUrl = new URL(navigateFallback, self.location);
+      cacheName = AbsoluteUrlToCacheName[navigateFallbackUrl.toString()];
+    }
+
+    if (cacheName) {
+      event.respondWith(
+        // We can't call cache.match(event.request) since the entry in the cache will contain the
+        // cache-busting parameter. Instead, rely on the fact that each cache should only have one
+        // entry, and return that.
+        caches.open(cacheName).then(function(cache) {
+          return cache.keys().then(function(keys) {
+            return cache.match(keys[0]).then(function(response) {
+              return response || fetch(event.request).catch(function(e) {
+                console.error('Fetch for "%s" failed: %O', urlWithoutIgnoredParameters, e);
+              });
+            });
+          });
+        }).catch(function(e) {
+          console.error('Couldn\'t serve response for "%s" from cache: %O', urlWithoutIgnoredParameters, e);
+          return fetch(event.request);
+        })
+      );
+    }
+  }
+});
 
